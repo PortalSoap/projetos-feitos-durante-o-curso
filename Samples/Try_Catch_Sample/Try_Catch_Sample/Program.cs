f5d3d5fa58a8b1d1c3b479d19cc98cea388548e8
Lucas M. Lacerda <lucas.lacerda.lg@gmail.com>
2021-08-01 03:03:51 -0300

    Adicionado Try_Catch_Sample

---
+++ Samples/Try_Catch_Sample/Try_Catch_Sample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Try_Catch_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            try // O bloco "try" deve ser aplicado onde possívelmente haverá um erro por parte do usuário.
+            {
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+
+                c = a / b;
+                Console.WriteLine("Result: " + c);
+            }
+            catch (DivideByZeroException) // O catch será o resultado (o que deverá ser feito) caso haja exceção. Há vários tipos.
+            {
+                Console.WriteLine("Divide by 0 is not alowed!");
+            }
+            catch(Exception x) // Caso seja do tipo de uma superclasse, deverá ser criada uma váriavel para chamar os métodos, e então uma mensagem padrão referente ao tipo de exceção será exibida.
+            {
+                Console.WriteLine("Error! " + x.Message);
+            }
+        }
+    }
+}
