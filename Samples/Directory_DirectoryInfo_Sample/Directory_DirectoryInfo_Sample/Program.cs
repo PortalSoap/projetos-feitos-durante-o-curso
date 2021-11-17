d4536d3bad22ef30ddbe1dd3c759554dbd04b9ae
Lucas M. Lacerda <lucas.lacerda.lg@gmail.com>
2021-11-17 01:10:41 -0300

    Adicionado DirectorY_DirectoryInfo_Sample e removido projeto antigo do jogo de xadrez, ao qual se encontra em um novo repositório.

---
+++ Samples/Directory_DirectoryInfo_Sample/Directory_DirectoryInfo_Sample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Directory_DirectoryInfo_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = @"c:\Temp\Myfolder";
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                // Os termos em "" representam máscaras de busca. "*.*" significa que qualquer nome de arquivo com qualquer extensão de arquivo pode participar da busca.
+                // Na prática, "*.*" significa qualquer coisa.
+                // O SearchOption.AllDirectories permite que seja realizada também uma busca dentro das possíveis subpastas nesse diretório. Assim, também vai ser listado arquivos de subpastas.
+
+                // O IEnumerable é o tipo mais genérico de coleção.
+                // Também é possível usar uma váriavel var para armazenar a coleção, reduzindo assim a sintaxe para chamá-la e dispensando também a necessidade da bibilioteca System.Collections.Generic.
+                Console.WriteLine("FOLDERS:");
+                foreach(string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Console.WriteLine();
+
+                // Aqui será feito a mesma coisa, mas com arquivos agora (trocando o Directory.EnumerateDirectories para Directory.EnumerateFiles).
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach(string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                // Agora será criado uma pasta no caminho especificado, com o uso do tipo Directory. Também é necessário nomear a pasta, tornando-a uma subpasta. Veja o exemplo:
+                Directory.CreateDirectory(path + @"\newfolder");
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error has ocurred.");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Samples/StreamWriter_Sample/StreamWriter_Sample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace StreamWriter_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string sourcePath = @"c:\Temp\File1.txt";
+                string targetPath = @"c:\Temp\File2.txt";
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                using (StreamWriter streamWriter = File.AppendText(targetPath)) // Mais um exemplo do using, que fecha toda e qualquer Stream que for colocado em sua atribuição assim que for terminada.
+                {
+                    foreach(string line in lines)
+                    {
+                        streamWriter.WriteLine(line.ToUpper()); // O StreamWritter, assim como o nome diz, tem a finalidade de escrever caractéres em arquivos.
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error has ocurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
