78ac79c5dca5b7d72f63424b588034d540dbe38d
Lucas M. Lacerda <lucas.lacerda.lg@gmail.com>
2021-11-28 03:51:58 -0300

    Adicionado Exercicio_sem_Inferface.

---
+++ Exercicio_sem_Interface/Exercicio_sem_Interface/Entities/Rent.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using System.Globalization;
+
+namespace Exercicio_sem_Interface.Entities
+{
+    class Rent
+    {
+        public string CarModel { get; set; }
+        public DateTime PickupDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        public Rent()
+        {
+        }
+
+        public Rent(string carModel, DateTime pickupDate,
+            DateTime returnDate, double pricePerHour, double pricePerDay)
+        {
+            CarModel = carModel;
+            PickupDate = pickupDate;
+            ReturnDate = returnDate;
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public double BasePayment()
+        {
+            TimeSpan duration =  ReturnDate.Subtract(PickupDate);
+            int hours = duration.Hours;
+            int days = duration.Days;
+            if(duration.Days < 1 && duration.Hours <= 12)
+            {
+                if(duration.Minutes > 0)
+                {
+                    hours++;
+                    
+                }
+                return hours * PricePerHour;
+            }
+            else
+            {
+                if(duration.Hours > 0)
+                {
+                    days++;
+                }
+                return days * PricePerDay;
+            }
+        }
+
+        public double TaxPayment()
+        {
+            double basePayment = BasePayment();
+            if(basePayment <= 100)
+            {
+                return 0.2 * basePayment;
+            }
+            else
+            {
+                return 0.15 * basePayment;
+            }
+        }
+
+        public double TotalPayment()
+        {
+            return BasePayment() + TaxPayment();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("INVOICE:");
+            sb.AppendLine("Basic payment: " + BasePayment().ToString("F2", CultureInfo.InvariantCulture));
+            sb.AppendLine("Tax: " + TaxPayment().ToString("F2", CultureInfo.InvariantCulture));
+            sb.AppendLine("Total payment: " + TotalPayment().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
---
+++ Exercicio_sem_Interface/Exercicio_sem_Interface/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+using Exercicio_sem_Interface.Entities;
+
+namespace Exercicio_sem_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string carModel;
+            double pricePerHour, pricePerDay;
+            DateTime pickupDate, returndate;
+
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            carModel = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyyy - HH:mm): ");
+            pickupDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yyyy - HH:mm): ");
+            returndate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Enter price per hour: ");
+            pricePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            pricePerDay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Rent r = new Rent(carModel, pickupDate, returndate, pricePerHour, pricePerDay);
+            Console.WriteLine(r);
+        }
+    }
+}
